121a358edd6ed2202f4721743ffa3c36fbe2f52c
Lenin Lucas <developer.leninjo@gmailc.om>
2021-05-05 20:56:38 -0600

    Commit para subir el codigo al repo

---
+++ main.js
@@ -0,0 +1,14 @@
+function buscarEquipos(){
+  contenedor = document.getElementById("equipo").value;
+
+  //url de las navieras 
+  cosco_url = "https://elines.coscoshipping.com/ebtracking/public/containers/";
+
+  // axios.get(cosco_url+contenedor)
+  // .then((response) => {
+  //   console.log(response.data);
+  // })
+  // .catch((error) => {
+  //   console.error(error);
+  // })
+}
