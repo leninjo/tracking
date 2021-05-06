bec190968df1446c7cc9d2b96f45da8862d361bd
Lenin Lucas <developer.leninjo@gmailc.om>
2021-05-05 21:01:12 -0600

    Se agregó el CDN de axios

--- main.js
+++ main.js
@@ -4,11 +4,11 @@ function buscarEquipos(){
   //url de las navieras 
   cosco_url = "https://elines.coscoshipping.com/ebtracking/public/containers/";
 
-  // axios.get(cosco_url+contenedor)
-  // .then((response) => {
-  //   console.log(response.data);
-  // })
-  // .catch((error) => {
-  //   console.error(error);
-  // })
+  axios.get(cosco_url+contenedor)
+  .then((response) => {
+    console.log(response.data);
+  })
+  .catch((error) => {
+    console.error(error);
+  })
 }
